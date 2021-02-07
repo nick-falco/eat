25109aa92c5bea0ea23cf6d9a8bbd6e95b303f84
Nicholas Falco <ncfalco@gmail.com>
2021-02-07 12:48:35 -0600

    Add valid term generator implementing the gamblers ruin algorithm

--- eat/core/utilities.py
+++ eat/core/utilities.py
@@ -1,5 +1,22 @@
+import string
 from itertools import combinations
 
 
 def subset_sums(array, target, length):
     return [c for c in combinations(array, length) if sum(c) == target]
+
+
+def get_term_elements(size):
+    """
+    Returns a list of term elements matching the specified size.
+    Term elments are selected from [a-z].
+
+    :type size: int
+    :param size: number of possible term elements
+
+    :rtype: list
+    :return: list of term elements
+    """
+    if size < 1:
+        raise ValueError("Row size must be a valid positive integer.")
+    return list(string.ascii_lowercase)[0:size]
