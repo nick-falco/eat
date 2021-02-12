c60a72082c0086b40fa909132e0bc7e109f8d296
Nicholas Falco <ncfalco@gmail.com>
2021-02-11 22:46:27 -0600

    - Get DDA to converge but produces incorrect solution - Add more tests

--- eat/core/tests/test_components.py
+++ eat/core/tests/test_components.py
@@ -9,9 +9,35 @@ class TestTermOperation(unittest.TestCase):
         grp.data = grp.list_to_groupoid_data([2, 1, 2,
                                               1, 0, 0,
                                               0, 0, 1])
-        to = TermOperation(grp, random_target=True)
+        to = TermOperation(grp)
         l_array = to.l_array([[0], [1], [0], [2]])
         self.assertEqual(l_array, [[1, 2], [0, 1, 2], [1, 2], [0]])
+
+    def test_r_array(self):
+        grp = Groupoid(3)
+        grp.data = grp.list_to_groupoid_data([2, 1, 2,
+                                              1, 0, 0,
+                                              0, 0, 1])
+        to = TermOperation(grp)
+        r_array = to.r_array([[0], [1], [0], [2]],
+                             [[0, 1], [0], [0, 1, 2], [0, 2]])
+        self.assertEqual(r_array, [[1], [1, 2], [0, 1, 2], [0, 1]])
+
+        r_array = to.r_array([[0], [1], [0], [2]],
+                             [[1], [0, 1, 2], [0, 1, 2], [0]])
+        self.assertEqual(r_array, [[1], [0, 1, 2], [0, 1, 2], [0, 1]])
+
+        r_array = to.r_array([[0], [1], [0], [2]],
+                             [[1, 2], [1], [1], [0]])
+        self.assertEqual(r_array, [[0, 1, 2], [0], [1], [0, 1]])
+
+    def test_is_solution(self):
+        grp = Groupoid(3)
+        to = TermOperation(grp)
+        self.assertTrue(to.is_solution([[0], [1], [0], [2]],
+                                       [[0], [1], [0], [2]]))
+        self.assertTrue(to.is_solution([[0], [1], [0], [2]],
+                                       [[0, 1, 2], [0, 1], [0, 1], [0, 2]]))
 
 
 if __name__ == "__main__":
