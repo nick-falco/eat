8b692f52c33c75cd12372ba3d6422bdb3a7a7b0c
Nicholas Falco <ncfalco@gmail.com>
2021-03-24 10:09:45 -0600

    Test utilities

---
+++ eat/core/tests/test_utilities.py
@@ -0,0 +1,9 @@
+import unittest
+from eat.core.utilities import split_male_term
+
+class TestUtilities(unittest.TestCase):
+
+    def test_split_male_term(self):
+        self.assertEqual(split_male_term("yx*yzx***"), ("yx*", "yzx**"))
+        self.assertEqual(split_male_term("xy*"), ("x", "y"))
+        self.assertEqual(split_male_term("xy*zx**"), ("xy*", "zx*"))
